44548e8027506d1ed80e46d4ae8996fc871f0651
Aleksandr Khrapal <khrapalsasha@gmail.com>
2018-08-08 16:31:06 +0300

    Admin dashboard

---
+++ js/admin-main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $('.sidebar-trigger').on('click', function (e) {
+       e.preventDefault();
+
+       var adminSidebar = $('.admin-sidebar');
+       var adminContainer = $('.admin-container');
+
+       if(adminSidebar.hasClass('sidebar-sm')) {
+           adminSidebar.removeClass('sidebar-sm');
+           adminContainer.removeClass('container-sm');
+       } else {
+           adminSidebar.addClass('sidebar-sm');
+           adminContainer.addClass('container-sm');
+       }
+    });
+});
